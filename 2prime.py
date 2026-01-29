705c34fb4e22675e7cfc0c623a391844ed79d7c7
unknown <thoutuakshaya1@gmail.com>
2026-01-29 22:14:37 +0530

    DAY1

--- 2prime.py
+++ 2prime.py
@@ -1,5 +1,9 @@
 # Problem Statement
-# In a programming contest, you get a question: determine whether a given number is prime. You remember a prime number is a number greater than 1 that has no divisors other than 1 and itself. To solve this, you decide to check divisibility from 2 up to the square root of the number. This way, you can quickly find if any divisor exists and decide primality.# n=int(input("number="))
+# In a programming contest, you get a question: determine whether a given number is prime.
+#  You remember a prime number is a number greater than 1 that has no divisors other than 1
+#  and itself. To solve this, you decide to check divisibility from 2 up to the square root 
+# of the number. This way, you can quickly find if any divisor exists and decide primality.
+# # n=int(input("number="))
 # for i in range(2,int(n**0.5)+1):
 #     if int(n%i)==0:
 #         print("Not Prime")
--- 3leap.py
+++ 3leap.py
@@ -1,5 +1,8 @@
 # Problem Statement
-# While developing a calendar app, you notice that some years have an extra day in February. You recall the leap year rule: a year is a leap year if it is divisible by 4, except if it is divisible by 100, unless it is also divisible by 400. To correctly mark these years, you decide to implement this logic and identify leap years.
+# While developing a calendar app, you notice that some years have an extra day
+#  in February. You recall the leap year rule: a year is a leap year if it is divisible by 4
+# , except if it is divisible by 100, unless it is also divisible by 400. To correctly mark these
+# years, you decide to implement this logic and identify leap years.
 
 
 n=int(input("year="))
