f1e2ab97bcaa3008c93c4ac29e6343b8eaa0e846
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-04-04 23:06:21 +0530

    Add files via upload

---
+++ celcius.py
@@ -0,0 +1,9 @@
+'''On a cold winter day, you want to
+ convert temperatures from Celsius to Fahrenheit
+to understand the weather better. Using the formula 
+F = (C × 9/5) + 32, you decide to implement a program
+ that performs this conversion for any given Celsius temperatur'''
+
+n=float(input())
+f=(n*9/5)+32
+print(f)
